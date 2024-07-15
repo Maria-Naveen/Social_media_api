5fa182c0efb8bb561e0663cc561f44356b9058bc
Maria Naveen <marianaveen15@outlook.com>
2024-07-16 00:12:26 +0530

    Created a route and controller for comment likes.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -8,6 +8,7 @@ import addComment from "../controllers/addComment.js";
 import toggleLike from "../controllers/toggleLike.js";
 import getPostDetails from "../controllers/getPostDetails.js";
 import updateComment from "../controllers/updateComment.js";
+import toggleCommentLike from "../controllers/toggleCommentLike.js";
 import deleteComment from "../controllers/deleteComment.js";
 const router = express.Router();
 
@@ -26,5 +27,10 @@ router.post("/posts/:id/comments", verifyUser, addComment);
 router.patch("/posts/:postId/toggle-like", verifyUser, toggleLike);
 router.get("/posts/:id", verifyUser, getPostDetails);
 router.patch("/posts/:postId/comments/:commentId", verifyUser, updateComment);
+router.patch(
+  "/posts/:postId/comments/:commentId/toggle-like",
+  authenticate,
+  toggleCommentLike
+);
 router.delete("/posts/:postId/comments/:commentId", verifyUser, deleteComment);
 export default router;
