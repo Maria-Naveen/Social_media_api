5d4f21beedc13e025a89f59353e27c440a6e5a14
Maria Naveen <marianaveen15@outlook.com>
2024-07-19 15:48:02 +0530

    Made port changes

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use("/api", postRoutes);
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
   console.log("Server started at port 3000");
 });
