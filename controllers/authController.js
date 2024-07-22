ffe7224be37b75c8335130c11aed7057eda25b1a
Maria Naveen <marianaveen15@outlook.com>
2024-07-22 17:37:09 +0530

    Tested the authController using jest

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,25 @@
 import userService from "../services/userService.js";
 import catchAsync from "../utils/catchAsync.js";
+import { ValidationError } from "../utils/customErrors.js";
 
 const signupController = catchAsync(async (req, res) => {
   const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new ValidationError("All fields are required.");
+  }
+
   const result = await userService.signup(name, email, password);
   res.status(201).json(result);
 });
 
 const loginController = catchAsync(async (req, res) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ValidationError("All fields are required.");
+  }
+
   const result = await userService.login(email, password);
   res.status(200).json(result);
 });
--- server.js
+++ server.js
@@ -21,3 +21,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server started at port 3000");
 });
+
+export default app;
